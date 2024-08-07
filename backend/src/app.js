d2d70d974628fa84d7989d232e215d2b94d1ca47
Wesley Godoy <wesley.godoy@lojasmm.com.br>
2024-08-07 20:03:13 -0300

    Feat: Configurando SPA

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,7 @@ const cors = require("cors");
 const swaggerFile = require('../swagger')
 const swaggerUI = require("swagger-ui-express");
 const swaggerJSDOC = require("swagger-jsdoc");
+const path = require('path');
 
 class AppController {
   constructor() {
@@ -22,12 +23,18 @@ class AppController {
     this.express.use(express.json());
     const specs = swaggerJSDOC(swaggerFile);
     this.express.use('/doc', swaggerUI.serve, swaggerUI.setup(specs));
+    this.express.use(express.static(path.join(__dirname, '../', 'build')));
   }
 
   routes() {
     this.express.use("/api/v1", require("./routes/index-routes"));
     this.express.use("/api/v1", require("./routes/login-routes"));
     this.express.use("/api/v1/user", require("./routes/user-routes"));
+
+    /** React */
+    this.express.get('/*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../', 'build', 'index.html'));
+    });
   }
 }
 
