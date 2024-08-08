d98968b8f25dd81ef4cd5f44c3707cf960493a8b
Wesley Godoy <wesley.godoy@lojasmm.com.br>
2024-08-08 12:42:02 -0300

    Feat [Front]: Http service

--- frontend/src/layouts/dashboards/teste/index.tsx
+++ frontend/src/layouts/dashboards/teste/index.tsx
@@ -1,7 +1,13 @@
 import MDBox from "components/MDBox";
+import MDButton from "components/MDButton";
 import MDTypography from "components/MDTypography";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import httpService from "services/http.service";
+
+const testando = async () => {
+  await httpService.get("/info");
+};
 
 function Teste() {
   const message = `Isso é apenas um teste pra você ver como é feito a tela, Chi. Depois esse componente deve ser excluído.`;
@@ -12,6 +18,7 @@ function Teste() {
         <MDTypography variant="h5" fontWeight="regular" sx={{ mt: 4 }}>
           {message}
         </MDTypography>
+        <MDButton onClick={testando}>{`Teste`}</MDButton>
       </MDBox>
     </DashboardLayout>
   );
