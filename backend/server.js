2113555413f738b8dd2ff909f4239aa97dfffe95
Wesley Godoy <wesley.godoy@lojasmm.com.br>
2024-08-09 14:13:53 -0300

    Teste vercel

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,15 @@
-const app = require('./src/app');
+// const app = require('./src/app');
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log('Aplicação executando na porta: ', port);
+// });
+const express = require('express');
+const app = express();
+
+app.get('/api/v1/teste', (req, res) => {
+  res.send('Rota de teste funcionando!');
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('Aplicação executando na porta: ', port);
