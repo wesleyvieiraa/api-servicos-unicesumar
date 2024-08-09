be9ec12b013ceee0aa0c33bfc2e74e5a68bff7e5
Wesley Godoy <wesley.godoy@lojasmm.com.br>
2024-08-09 14:33:35 -0300

    Fix [Back]: Retornando server js

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,4 @@
-// const app = require('./src/app');
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => {
-//   console.log('Aplicação executando na porta: ', port);
-// });
-const express = require('express');
-const app = express();
-
-app.get('/api/v1/teste', (req, res) => {
-  res.send('Rota de teste funcionando!');
-});
-
+const app = require('./src/app');
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('Aplicação executando na porta: ', port);
