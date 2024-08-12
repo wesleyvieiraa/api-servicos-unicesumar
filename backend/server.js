1ec6dd999b6b79b466045516fe82572ed307f958
Wesley Godoy <wesley.godoy@lojasmm.com.br>
2024-08-12 11:22:32 -0300

    vercel

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,21 @@
-const app = require('./src/app');
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log('Aplicação executando na porta: ', port);
-});
+// const app = require('./src/app');
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log('Aplicação executando na porta: ', port);
+// });
+
+
+require("dotenv").config();
+
+const express = require("express");
+const PORT = 3000;
+const app = express();
+app.use(express.json());
+
+app.get("/api/v1/teste", async (req, res) => {
+  return res.status(200).send("Teste OK");
+})
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+})
