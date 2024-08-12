9ca25027fc9be9a59069829fba7ad9026fc742ae
Wesley Godoy <wesley.godoy@lojasmm.com.br>
2024-08-12 11:40:43 -0300

    Retornando app

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,5 @@
-// const app = require('./src/app');
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => {
-//   console.log('Aplicação executando na porta: ', port);
-// });
-
-
-require("dotenv").config();
-
-const express = require("express");
-const PORT = 3000;
-const app = express();
-app.use(express.json());
-
-app.get("/api/v1/teste", async (req, res) => {
-  return res.status(200).send("Teste OK");
-})
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-})
+const app = require('./src/app');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('Aplicação executando na porta: ', port);
+});
