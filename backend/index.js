65c80607d40b5ba5ef599ddd8f41a78c5d7e94dd
Wesley Godoy <wesley.godoy@lojasmm.com.br>
2024-08-12 12:03:48 -0300

    Alterando pra index

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-const app = require('./src/app');
+const app = require('./src');
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('Aplicação executando na porta: ', port);
