08e399668bc65808e5da5152c756d0de108f69c1
Wesley Godoy <wesley.godoy@lojasmm.com.br>
2024-08-13 09:28:09 -0300

    Feat [Front]: Navigate new services

--- frontend/src/layouts/Dashboard/Dashboard.tsx
+++ frontend/src/layouts/Dashboard/Dashboard.tsx
@@ -3,18 +3,21 @@ import MDBox from "components/MDBox";
 import ComplexStatisticsCard from "examples/Cards/StatisticsCards/ComplexStatisticsCard";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import { useNavigate } from "react-router-dom";
 
 export const Dashboard = (): JSX.Element => {
-  const teste = () => {
-    console.log("eu");
+  const navigate = useNavigate();
+  const nav = () => {
+    navigate("/new-service");
   };
+
   return (
     <DashboardLayout>
       <DashboardNavbar />
       <MDBox mt={1.5}>
         <Grid container spacing={3}>
           <Grid item xs={12} md={6} lg={3}>
-            <MDBox mb={1.5} onClick={teste} sx={{ cursor: "pointer" }}>
+            <MDBox mb={1.5} onClick={nav} sx={{ cursor: "pointer" }}>
               <ComplexStatisticsCard
                 color="dark"
                 icon="weekend"
